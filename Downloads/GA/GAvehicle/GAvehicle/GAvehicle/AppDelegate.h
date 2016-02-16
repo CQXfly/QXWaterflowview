1f550d5ff57656080c06fa95f92467b3dbce59d0
huangWay <hw758214335@163.com>
2016-02-16 13:24:14 +0800

    init GA

---
+++ Downloads/GA/GAvehicle/GAvehicle/GAvehicle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GAvehicle
+//
+//  Created by HuangWay on 16/2/16.
+//  Copyright © 2016年 HuangWay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Downloads/GA/GAvehicle/GAvehicle/GAvehicle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GAvehicle
+//
+//  Created by HuangWay on 16/2/16.
+//  Copyright © 2016年 HuangWay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
